9de8f742d1ac538c351173e5d5d7890302046c8c
Eze Heredia <eze.heredia023@gmail.com>
2020-01-14 18:46:31 -0300

    Update clase autos 3.0.py

--- Ejercicios 6 Objetos/clase autos 3.0.py
+++ Ejercicios 6 Objetos/clase autos 3.0.py
@@ -10,3 +10,6 @@ class Marca(Auto):
 
 auto1=Marca("VW","Gol")
 auto1.salida()
+
+#creamos una clase auto con una funcion de salida la cual concatenara una herencia de la clase Marca cuya funcion esta inicializada para tomar los datos ingresados
+#la salida sera la cadena de la funcion salida
