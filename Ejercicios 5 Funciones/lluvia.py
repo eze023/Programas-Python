ccfee7c65ff8cc69ee3c5eb8ffb047185c749c71
Eze Heredia <eze.heredia023@gmail.com>
2020-01-14 18:46:18 -0300

    Update lluvia.py

--- Ejercicios 5 Funciones/lluvia.py
+++ Ejercicios 5 Funciones/lluvia.py
@@ -26,3 +26,5 @@ agua, dias, total=diassemana()
 dia, cantidad=lluvia(agua, dias)
 
 print("El dia que mas llovio fue el ", dia, "con un total de", cantidad, "y en la semana llovio",total, "mm")
+
+#el programa utiliza una funcion para determinar el dia que mas llovio y el total de milimetros caidos en la semana
