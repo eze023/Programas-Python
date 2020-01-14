c550d3b20a91559cf5a4a228bb138e7ded4729ff
Eze Heredia <eze.heredia023@gmail.com>
2020-01-14 18:44:37 -0300

    Update pasaje.py

--- Ejercicios 1 Basico/pasaje.py
+++ Ejercicios 1 Basico/pasaje.py
@@ -6,14 +6,17 @@ nombre=str(input("Ingrese su nombre: "))
 edad=int(input("Ingrese su edad: "))
 
 if (edad>=5 and edad<=10):
-	pasaje=pasaje*40
-	pasaje=pasaje/100
-	pasaje=400-pasaje
+	descuento=100*0.4
+	total=pasaje - descuento
 elif (edad>=65):
-	pasaje=pasaje*40
-	pasaje=pasaje/100
-	pasaje=400-pasaje
+	descuento=100*0.4
+	total=pasaje - descuento
 else:
-	pasaje=400
+	total=pasaje
 
-print("Nombre: ",nombre," Edad: ",str(edad),"Valor del pasaje: ",str(pasaje))
+print("Nombre: ",nombre," Edad: ",str(edad),"Valor del pasaje: ",str(total
+))
+
+#programa que aplica descuento en el pasaje de un viaje siempre y cuando sea de edad mayor a 5 y menor a 10
+#o que sea de una persona mayor a 65 años
+#en caso contrario se cobrara el valor total del boleto
