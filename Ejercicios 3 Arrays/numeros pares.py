e4b8a83903ea40f23210c0acfb52e0908417c38b
Eze Heredia <eze.heredia023@gmail.com>
2020-01-14 18:47:23 -0300

    Update numeros pares.py

--- Ejercicios 3 Arrays/numeros pares.py
+++ Ejercicios 3 Arrays/numeros pares.py
@@ -8,3 +8,5 @@ for i in range(0,31):					#Proceso
 #Salida
 print(lista[:])
 print("Fin del programa")
+
+#el programa a partir de un loop generando numeros de 0 a 31 muestra en pantalla los numeros pares
