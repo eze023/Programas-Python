781174700f48d14dc701e1afffa8e1a2bfc94714
Eze Heredia <eze.heredia023@gmail.com>
2020-01-14 18:45:27 -0300

    Update cantidad de letras.py

--- Ejercicios 4 Strings/cantidad de letras.py
+++ Ejercicios 4 Strings/cantidad de letras.py
@@ -14,4 +14,6 @@ for i in cadena1:
 	if len(i)>len(palabra):		#len(i)= i equivale a la palabra
 		palabra=i
 
-print(len(palabra))
+print("La palabra mas larga contiene",len(palabra),"letras")
+
+#el programa determina la cantidad de letras que contiene la palabra mas larga, compara en un bucle entre todas las palabras y luego una vez determinada comenzara a contar las letras en dicha palabra
