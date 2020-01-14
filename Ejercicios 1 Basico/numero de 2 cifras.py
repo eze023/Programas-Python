1fc3d08557969f76858d3ce8f4340c9e58414fa5
Eze Heredia <eze.heredia023@gmail.com>
2020-01-14 18:44:33 -0300

    Update numero de 2 cifras.py

--- Ejercicios 1 Basico/numero de 2 cifras.py
+++ Ejercicios 1 Basico/numero de 2 cifras.py
@@ -5,7 +5,12 @@ num1=int(input("Ingrese el numero a calcular: "))
 
 if 9<num1<100:
 	print("El numero es positivo y de dos digitos: ",str(num1))
-elif num1<0:
+elif (num1<0) and (num1>-10):
 	print("El numero es negativo: ",str(num1))
+elif num1<-9:
+	print("El numero es negativo de dos cifras", str(num1))
 else:
 	print("El numero es superior a dos digitos: ",str(num1))
+
+#programa cuya finalidad es mostrar que tipo de numero es, si es positivo o negativo
+#en caso de ser positivo o negativo mostrar si es de 1 cifra, 2 o mas cifras
