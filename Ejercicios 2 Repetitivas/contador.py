e855877416ffd2ef0255d6b861b936f8af904bdc
Eze Heredia <eze.heredia023@gmail.com>
2020-01-14 18:44:53 -0300

    Update contador.py

--- Ejercicios 2 Repetitivas/contador.py
+++ Ejercicios 2 Repetitivas/contador.py
@@ -8,3 +8,7 @@ for i in range(10):
 
 print("Hay ", contador," Numeros mayores que 23")
 print("Fin del programa")
+
+#el programa pedira ingresar 10 numeros
+#aquellos numeros que sean superiores a 23 seran cargados a un contador
+#una vez finalizado este mostrara la cantidad de numeros que son mayores a 23
