c0872317c48fd838391de4f21f95fc779986d9ad
Eze Heredia <eze.heredia023@gmail.com>
2020-01-14 18:44:27 -0300

    Update numeracion.py

--- Ejercicios 1 Basico/numeracion.py
+++ Ejercicios 1 Basico/numeracion.py
@@ -7,3 +7,6 @@ while num1==0:
 	num1=int(input("Ingrese un numero: "))
 
 print("Numero distinto de cero: ",num1)
+
+#Programa basico de control
+#Si el numero es distinto de 0 termina el programa en caso contrario seguira pidiendo numeros
