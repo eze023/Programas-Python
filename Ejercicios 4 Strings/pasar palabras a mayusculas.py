fd90f790d00372a24c82d16e946abde901972214
Eze Heredia <eze.heredia023@gmail.com>
2020-01-14 18:45:48 -0300

    Update pasar palabras a mayusculas.py

--- Ejercicios 4 Strings/pasar palabras a mayusculas.py
+++ Ejercicios 4 Strings/pasar palabras a mayusculas.py
@@ -9,3 +9,6 @@ for i in range(len(pal)):
 	pal2+=chr(letra)
 
 print(pal2)
+
+#Ingresamos una palabra por consola y a traves del programa transforma la palabra a mayusculas
+#a pesar de que python tiene una funcion la cual transforma la palabras a mayusculas nos hacemos valer por una funcion generada para realizar la misma funcion
