9ddadacd32409cc001ee367257c3240d104f8db2
Eze Heredia <eze.heredia023@gmail.com>
2020-01-14 18:44:43 -0300

    Update sueldo.py

--- Ejercicios 1 Basico/sueldo.py
+++ Ejercicios 1 Basico/sueldo.py
@@ -4,17 +4,17 @@ print()
 dias=int(input("Print escriba la cantidad de dias no trabajados: "))
 anio=int(input("Ingrese el año de ingreso a la empresa: "))
 sueldo=23000
-porcentaje=0
-actualidad=2019
+actualidad=2020
 
 actualidad=actualidad-anio
 
 if (dias==0 and actualidad>=5):
-	porcentaje=sueldo*30
-	porcentaje=porcentaje/100
+	porcentaje=sueldo*0.3
 	sueldo=sueldo+porcentaje
 	print("Su sueldo es: ", int(sueldo))
 else:
 	print("Su sueldo es: ",int(sueldo))
 
 print("Fin del programa")
+
+#Calcula el adicional al sueldo si este no falto ningun dia al trabajo y su tiempo de trabajo es superior a 5 años en la empresa
