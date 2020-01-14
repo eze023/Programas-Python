04ec906b2a5745a0882ff3da83acbe3c171f0f2f
Eze Heredia <eze.heredia023@gmail.com>
2020-01-14 18:46:15 -0300

    Update invertirlista.py

--- Ejercicios 5 Funciones/invertirlista.py
+++ Ejercicios 5 Funciones/invertirlista.py
@@ -1,4 +1,3 @@
-print("Ejercicio 10")
 
 def invertirlista(lista):
 	lista.reverse()
@@ -9,3 +8,5 @@ lista=[1,2,3,4,5,6,7,8,9,0]
 print(lista)
 print()
 print("La lista invertida es: ", invertirlista(lista))
+
+#el programa utiliza una funcion para revertir la lista predeterminada
