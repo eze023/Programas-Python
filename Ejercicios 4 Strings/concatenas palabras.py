36d9b21e77b085c276988ffd47800030d35f1d44
Eze Heredia <eze.heredia023@gmail.com>
2020-01-14 18:45:30 -0300

    Update concatenas palabras.py

--- Ejercicios 4 Strings/concatenas palabras.py
+++ Ejercicios 4 Strings/concatenas palabras.py
@@ -10,3 +10,5 @@ final=cadena[posicion:]
 total=principio+cadena2+final
 
 print(total)
+
+#el programa de dos cadenas las concatenara a partir de una posicion determinada y formara la frase que se muestra en pantalla
