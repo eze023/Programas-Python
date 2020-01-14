7856d5f56bd2f987f4c0101b37fa587f34810caf
Eze Heredia <eze.heredia023@gmail.com>
2020-01-14 18:44:35 -0300

    Update numero positivo - negativo.py

--- Ejercicios 1 Basico/numero positivo - negativo.py
+++ Ejercicios 1 Basico/numero positivo - negativo.py
@@ -3,12 +3,18 @@ print()
  
 num1 = int(input("Ingrese su numero: "))
 
-if num1>=0:
+if num1>0:
 	print()
-	print("Su numero es mayor o igual a 0")
-else:
+	print("Su numero es positivo")
+elif num1<0:
 	print()
 	print("Su numero es negativo")
+else:
+	print("Su numero es 0")
 
 print()
 print("Fin del programa")
+
+#programa basico sin control de ingreso
+#si el numero ingresado es mayor a 0 es positivo, si es menor a 0 es negativo
+#en caso de no cumplir ninguna de las dos operaciones logicas el numero sera 0
