cd9e3cc5e8114077f6444c96ab2f688dbf9c2a3a
Eze Heredia <eze.heredia023@gmail.com>
2020-01-14 18:45:58 -0300

    Update busqueda de letras.py

--- Ejercicios 5 Funciones/busqueda de letras.py
+++ Ejercicios 5 Funciones/busqueda de letras.py
@@ -1,5 +1,3 @@
-print("Ejercicio 1")
-
 def repeticion(cade, letra):
 	contador=0
 	for i in range(len(cade)):
@@ -14,4 +12,6 @@ print(cadena)
 print()
 letra=str(input("Ingrese una letra que desea buscar: "))
 
-print(repeticion(cade, letra))
+print("Las veces que aparece la letra es: ",repeticion(cade, letra))
+
+#el programa realiza la funcion de buscar y contar la cantidad de veces que se repite la letra en la frase ya preestablecida
