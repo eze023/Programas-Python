6807edea18aaa4104740742fd8ba0548dfcef939
Eze Heredia <eze.heredia023@gmail.com>
2020-01-14 18:45:33 -0300

    Update contador sin espacios.py

--- Ejercicios 4 Strings/contador sin espacios.py
+++ Ejercicios 4 Strings/contador sin espacios.py
@@ -9,3 +9,6 @@ for i in range(len(cadena)):
 
 print("Cadena queda asi:", nueva)
 print("La cantidad de caracteres es de:",len(nueva))
+
+#a partir de una cadena determinada en una variable, el programa determinara la cantidad de letras y caracteres ingresados en la cadena
+#esta cuenta se realizara sin espacios
